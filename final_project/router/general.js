3c1ef407e4ec5a0cd969869834502ca033c4bd9f
Francisco López García Mayorga <javier6282@hotmail.com>
2024-11-06 00:23:05 -0500

    getting the book details based on the author.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,8 +22,11 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  let filtered_author = Object.keys(books).find(
+    (key) => books[key].author === author
+  );
+  res.send(JSON.stringify(books[filtered_author], null, 2));
 });
 
 // Get all books based on title
